564622181fabdc91805af822c59015dcbb2be04a
Luis Castillo <lcastillo0692@gmail.com>
2018-11-04 08:24:43 -0500

    reloj
    se ha agregado la hora actual.

--- pagina/src/components/Content.js
+++ pagina/src/components/Content.js
@@ -1,91 +1,42 @@
 import React, { Component } from 'react';
 
 class Content extends Component {
-  constructor() {
-    super()
+  constructor(props) {
 
-     this.state = {
-      tiempo: new Date().toLocaleTimeString()
-    
-
-    
-    }
-
-
-// setInterval(this.state.tiempo, 1000);
-     
+    super(props);
+    this.state = {date: new Date()};
   }
 
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
 
-  
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
 
   render() {
 
-    return (
-      
-      
+        return (
       <div>
-          <h1>Hola colega</h1>
-          <h2>la hora es   {this.state.tiempo}.</h2>
-        </div>  
-        
-        
-    
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
     );
 
   }
 
-
-
-componentDidMount() {
-
-  
-
- // function tick() {
-
- //      this.setState({
- //        tiempo: 
- //      })
- //     }
-     // setInterval(tick, 1000);
-  }
 }
 
+
+
 export default Content;
-
-
-  //Contador
-  // constructor(){
-  //   super()
-    
-  //   this.state = {
-  //     contador: 0
-  //     }
-  //     this.handleInc = this.handleInc.bind(this);
-  //     this.handleDec = this.handleDec.bind(this);
-  // }
-
-  //   handleInc() {
-  //     this.setState({
-  //       contador: this.state.contador + 1
-  //     });
-  //   }
-
-  //   handleDec() {
-  //     this.setState({
-  //       contador: this.state.contador - 1
-  //     });
-  //   }
-
-  // render() {
-  //   return (
-  //     <div className="Content">
-  //     <h1>Cuenta: {this.state.contador}</h1>
-  //     <button onClick={this.handleInc}>+</button>
-  //     <button onClick={this.handleDec}>-</button>
-  //     </div>
-  //   );
-  // }
-
-
-
